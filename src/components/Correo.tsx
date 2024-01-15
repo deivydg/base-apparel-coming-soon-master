34ec884d53394111817c0c307e5614eb2ec79eb1
Deivyd Gomez Muñoz <deivydgomezm@gmail.com>
2024-01-15 08:07:35 -0500

    se agrega el proyecto

--- src/components/Correo.tsx
+++ src/components/Correo.tsx
@@ -1,35 +1,42 @@
 import React, { useState } from 'react';
 
 function Correo() {
+  // Expresión regular para validar el formato del correo electrónico
   const expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
+  // Estados para manejar el valor del correo, su validez y mostrar el mensaje de error
   const [correo, setCorreo] = useState('');
   const [esValido, setEsValido] = useState(false);
   const [mostrarMensaje, setMostrarMensaje] = useState(false);
 
+  // Manejador de cambio del campo de correo electrónico
+  //@ts-ignore
   const handleCorreoChange = (event) => {
     const nuevoCorreo = event.target.value;
     setCorreo(nuevoCorreo);
     setEsValido(expReg.test(nuevoCorreo));
-    setMostrarMensaje(false);
+    setMostrarMensaje(false); // Ocultar el mensaje de error cuando se realiza un cambio
   };
 
+  // Manejador de envío del formulario
   const handleSubmit = () => {
     console.log('Correo:', correo);
-    setMostrarMensaje(!esValido);
+    setMostrarMensaje(!esValido); // Mostrar el mensaje si el correo no es válido
   };
 
   return (
     <div className='containerCorreo'>
       <br />
-      
+      {/* Entrada de texto para el correo electrónico */}
       <input
         type="text"
         id="correo"
         onChange={handleCorreoChange}
         placeholder='Email Address'
+        // Clase condicional para agregar 'error' al input cuando hay un error
         className={`correoText ${mostrarMensaje && !esValido ? 'error' : ''}`}
       />
+      {/* Botón de envío con un ícono */}
       <button onClick={handleSubmit} className='BotonCorreo'>
         <img src="/src/assets/icon-arrow.svg" alt="" />
       </button>
